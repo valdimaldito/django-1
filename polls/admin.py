c0f36a9d7a16e873bfc5f7d274e665bd9c16d0f2
valdimaldito <dlozano1@xtec.cat>
2023-06-20 18:23:34 +0200

    millores

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,13 @@ from .models import Question
 from .models import Choice
 from .models import Vote
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    
+
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Vote)
--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # was_published_recently.boolean = True
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
