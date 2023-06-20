1dceea399d2058a02392d7ca9b0178021804285c
david <david@Ubuntu-David.myguest.virtualbox.org>
2023-06-20 15:07:13 +0200

    inici

---
+++ polls/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.choice_text
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return self.choice.choice_text + " - " + self.date.strftime("%m/%d/%Y, %H:%M:%S")
